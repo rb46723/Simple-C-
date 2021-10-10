876520146054848c275a7eeb5f1ce4fbaa06cd62
rb46723 <92258711+rb46723@users.noreply.github.com>
2021-10-10 18:51:07 +0530

    Create Program 6 - Arg.cpp

---
+++ Program 6 - Arg.cpp
@@ -0,0 +1,13 @@
+#include<process.h>
+#include<conio.h>
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+FILE *fin,*fout; char c;
+
+if(argc!=3)
+{
+printf("\n insufficient argument "); exit(1);
+}
+fin=fopen(argv[1],"r");
+fout=fopen(argv[2],"w");
