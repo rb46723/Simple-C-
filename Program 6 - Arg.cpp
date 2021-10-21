3c32844648db5b9e8e07585b6f2d40469c24decd
Abhishake-ak1 <92870961+Abhishake-ak1@users.noreply.github.com>
2021-10-21 01:14:37 -0700

    Update Program 6 - Arg.cpp

--- Program 6 - Arg.cpp
+++ Program 6 - Arg.cpp
@@ -5,17 +5,6 @@ int main(int argc,char *argv[])
 {
 FILE *fin,*fout; char c;
 
-if(argc!=3)
-{
-printf("\n insufficient argument "); exit(1);
-}
-fin=fopen(argv[1],"r");
-fout=fopen(argv[2],"w");
-
-while(!feof(fin))
-{
-c=fgetc(fin); fputc(c,fout);
-}
 
 fclose(fin); fclose(fout); return 0;
 
